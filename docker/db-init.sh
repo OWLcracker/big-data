5139b9babedfa939317866ec7caa3c5968ec364e
Kempf <kilian.kempf@sap.com>
2023-12-25 10:46:28 +0100

    mounted postgres (use wsl)

--- docker/db-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Custom code to load the SQL dump
-echo "Loading SQL dump..."
-psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} < /usr/local/bin/dump.sql
-
-# Call the original entrypoint script
-exec docker-entrypoint.sh postgres
